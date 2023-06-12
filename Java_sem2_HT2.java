63b3bbca1114e03088b43053d30a197335a4ddd9
Nikolay Borovik <nik.Borovik@bk.ru>
2023-06-12 16:00:19 +0100

    2done

--- Java_sem2_HT2.java
+++ Java_sem2_HT2.java
@@ -2,6 +2,7 @@ package Java_Seminar2.Java_Sem2_HomeTasks;
 
 import java.io.FileWriter;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
 public class Java_sem2_HT2 {
     /**
@@ -15,22 +16,24 @@ public class Java_sem2_HT2 {
         // 1 2 3 4
 
         try {
-            FileWriter file = new FileWriter("file.txt", false);
+            FileWriter file = new FileWriter("log.txt");
             int[]arr = {3,4,2,1};
+            int count = 0;
             for (int i = 0; i < arr.length-1; i++) {
                 for (int j = 0; j < arr.length-1; j++) {
                     if (arr[j] > arr[j+1]) {
                         int temp = arr[j];
                         arr[j] = arr[j+1];
                         arr[j+1] = temp;
-                        file.write("");
+                        count++;
+                        // file.write(count);
+                        file.write(count + " iteration: " + Arrays.toString(arr) + "\n");
+                    }
                 }
             }
+            file.close();
         }
-        System.out.println(Arrays.toString(arr));
-            writer.close();
-
-        }
+        
         catch (Exception e) {
             System.out.println("Smth went wrong!");
         }
